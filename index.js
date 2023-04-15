f87ef7cce022a3b7de0c8503dc50e2582c162197
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-04-15 13:55:54 -0700

    added 404 message

--- index.js
+++ index.js
@@ -3,7 +3,12 @@ const express= require('express')
 const app = express()
 
 app.get('/', function(req,res){
-    res.send()
+    res.send('Hello World!')
 })
+
+app.get('*', function(req,res){
+    res.send('<h1>404 Error, unfortunately the page you are trying to access does not exisit</h1>')
+})
+
 
 app.listen(process.env.PORT)
