ef53a95a0bc90a6eeb0f13f40173e0aeff8e3a31
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-04-15 14:14:44 -0700

    added readme file

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ require('dotenv').config()
 const express= require('express')
 const app = express()
 
-app.get('/', function(req,res){
+app.use('/places', require('./controllers/places'))
+
+
+app.get('/', (req,res)=>{
     res.send('Hello World!')
 })
 
-app.get('*', function(req,res){
+app.get('*', (req,res)=>{
     res.send('<h1>404 Error, unfortunately the page you are trying to access does not exisit</h1>')
 })
 
