94dd71259ab0cc78b8d043b58a3f1d29eed9612d
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-04-18 19:00:55 -0700

    Added home page view

--- index.js
+++ index.js
@@ -2,11 +2,16 @@ require('dotenv').config()
 const express= require('express')
 const app = express()
 
+//defines the view engine
+app.set('view engine', 'jsx')
+app.engine('jsx', require('express-react-views').createEngine())
+
+
 app.use('/places', require('./controllers/places'))
 
 
 app.get('/', (req,res)=>{
-    res.send('Hello World!')
+    res.render('home')
 })
 
 app.get('*', (req,res)=>{
