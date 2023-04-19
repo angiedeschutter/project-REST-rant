f3a9fde43b6d998df15247a37226e11552c27f44
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-04-18 19:04:21 -0700

    added 404 page view

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.get('/', (req,res)=>{
 })
 
 app.get('*', (req,res)=>{
-    res.send('<h1>404 Error, unfortunately the page you are trying to access does not exisit</h1>')
+    res.render('error404')
 })
 
 
