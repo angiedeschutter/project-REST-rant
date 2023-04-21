f5505b2b89110581f5f6654f8a346a314c218b72
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-04-20 19:51:32 -0700

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,18 +1,5 @@
 const router = require('express').Router()
-
-let places = [{
-    name: 'H-Thai-ML',
-    city: 'Seattle',
-    state: 'WA',
-    cuisines: 'Thai, Pan-Asian',
-    pic: '/images/jay-wennington-N_Y88TWmGwA-unsplash.jpg'
-  }, {
-    name: 'Coding Cat Cafe',
-    city: 'Phoenix',
-    state: 'AZ',
-    cuisines: 'Coffee, Bakery',
-    pic: '/images/zachariah-hagy-MCKrW7fB7Pw-unsplash.jpg'
-  }]
+const places = require('../models/place.js')
 
 router.get('/new', (req,res)=>{
     res.render('places/new')
@@ -23,9 +10,22 @@ router.get('/', (req,res)=>{
 })
 
 router.post('/', (req, res) => {
-  console.log(req.body)
-  res.send('POST /places')
+  //console.log(req.body)
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = 'http://placekitten.com/400/400'
+  }
+  if (!req.body.city) {
+    req.body.city = 'Anytown'
+  }
+  if (!req.body.state) {
+    req.body.state = 'USA'
+  }
+  places.push(req.body)
+  res.redirect('/places')
 })
+
+
 
 
 module.exports=router
---
+++ models/place.js
@@ -0,0 +1,14 @@
+
+module.exports = [{
+    name: 'H-Thai-ML',
+    city: 'Seattle',
+    state: 'WA',
+    cuisines: 'Thai, Pan-Asian',
+    pic: '/images/jay-wennington-N_Y88TWmGwA-unsplash.jpg'
+  }, {
+    name: 'Coding Cat Cafe',
+    city: 'Phoenix',
+    state: 'AZ',
+    cuisines: 'Coffee, Bakery',
+    pic: '/images/zachariah-hagy-MCKrW7fB7Pw-unsplash.jpg'
+  }]
