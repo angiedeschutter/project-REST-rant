0f832b56adfd75a2f7f90bfdbc147216df021c2a
Angiegit config --global user.name 'Angie <adeschutter6437@gmail.com>
2023-04-22 12:32:07 -0700

    added edit and delete buttons

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
 require('dotenv').config()
 const express= require('express')
 const app = express()
+const methodOverride = require('method-override')
 
 //defines the view engine
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true }))
-
 app.use('/places', require('./controllers/places'))
-
+app.use(methodOverride('_method'))
 
 app.get('/', (req,res)=>{
     res.render('home')
--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,31 @@
 const React = require('react')
 const Def = require('../default')
 
-function show () {
-    return (
-        <Def>
-          <main>
-            <h1>Show Page</h1>
-          </main>
-        </Def>
-    )
+function show(data) {
+  return (
+    <Def>
+      <main>
+        <h1>{data.place.name}</h1>
+        <h2>Rating</h2>
+        <h3>Not Rated</h3>
+        <h2>Description</h2>
+        <h3>Located in ...</h3>
+        <div>
+          <h2>Comments</h2>
+          <h3>No comments Yet</h3>
+        </div>
+      </main>
+      <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+        Edit
+      </a>
+      <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+        <button type="submit" className="btn btn-danger">
+          Delete
+        </button>
+      </form>
+
+    </Def>
+  )
 }
 
 module.exports = show
